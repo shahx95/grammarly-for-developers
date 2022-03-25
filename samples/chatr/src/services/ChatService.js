e0f1dd596fb7962817c14659bd9629a8d11903e4
Jason Sperske <jason.sperske@grammarly.com>
2022-03-25 16:01:57 -0700

    added more chats

--- samples/chatr/src/services/ChatService.js
+++ samples/chatr/src/services/ChatService.js
@@ -1,4 +1,5 @@
 const PEOPLE = {
+  0: { name: "You" },
   1: { name: "David H" },
   2: { name: "Rahul K" },
   3: { name: "Jason S" },
@@ -11,8 +12,32 @@ const CHATS = {
         body: "I don't have a strong feeling about how to describe our product launch!",
       },
       {
-        author: 2,
+        author: 0,
         body: "I know just the site to help you get unstuck.",
+      },
+    ],
+  },
+  2: {
+    messages: [
+      {
+        author: 0,
+        body: "Did you push the latest changes to our repo?",
+      },
+      {
+        author: 2,
+        body: "I'm about to, but I want some feedback before I push these into production, do you have a moment to chat?",
+      },
+    ],
+  },
+  3: {
+    messages: [
+      {
+        author: 0,
+        body: "Everything looks great, but are we sure we want to change the direction of our marking so dramatically?",
+      },
+      {
+        author: 3,
+        body: "Our customers are ready for something new, but I understand this is a big change, let's get a meeting with Rob to see if there is any new feedback from our beta users.",
       },
     ],
   },
