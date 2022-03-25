2d6236d8aaa046aff49bd50b80d5f9c770941646
Rahul Kadyan <rahul.kadyan@grammarly.com>
2022-03-25 16:10:15 -0700

    add chat list

--- samples/chatr/src/components/ChatList.jsx
+++ samples/chatr/src/components/ChatList.jsx
@@ -1,10 +1,30 @@
-import Button from "@mui/material/Button";
+import {
+  Button,
+  TextareaAutosize,
+  Card,
+  Stack,
+  CardContent,
+} from "@mui/material";
 
 export function ChatList() {
   return (
     <div className="ChatList">
-      This is the chat list
-      <Button variant="text">Text</Button>
+      <Card>
+        <CardContent>
+          <Stack>
+            <TextareaAutosize
+              minRows={4}
+              maxRows={10}
+              style={{ resize: "none" }}
+            />
+            <Stack direction="row" mt={2} justifyContent="flex-end">
+              <Button aria-label="send" variant="contained">
+                Send
+              </Button>
+            </Stack>
+          </Stack>
+        </CardContent>
+      </Card>
     </div>
   );
 }
