587febcad6eeb0a3c26414f2aa4ca8f59786cbf8
Lauren Schaefer <lauren.schaefer@grammarly.com>
2022-09-15 15:17:49 -0400

    Add demo vue app (#163)
    * Add demo vue app
    
    * Update CodeSandbox link
    
    * Update css to match other demo apps

---
+++ examples/demo-vue/src/components/Editors.vue
@@ -0,0 +1,20 @@
+<script>
+import { ref } from "vue";
+import { demoText } from "./demo";
+
+export default {
+  setup: () => {
+    const text = ref(demoText);
+    return { text };
+  },
+  components: {},
+};
+</script>
+
+<template>
+  <div>
+    <h1>Demo Vue App</h1>
+
+    <textarea v-model="text" rows="10"></textarea>
+  </div>
+</template>
